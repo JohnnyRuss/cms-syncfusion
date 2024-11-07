5698786f31f00c7746a8505b9482da933ac429d8
JohnnyRuss <georgiaspontoeli123@gmail.com>
2024-11-07 20:42:54 +0400

    * fix: z-index conflict between main navigation and suspense loader

--- src/components/Layouts/Spinner/Spinner.tsx
+++ src/components/Layouts/Spinner/Spinner.tsx
@@ -5,7 +5,7 @@ const Spinner: React.FC = () => {
   const { currentColor } = useAppContext();
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-50 backdrop-blur-sm scroll-block">
+    <div className="fixed inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.5)] z-[99999] backdrop-blur-sm scroll-block">
       <div className="loader" style={{ background: currentColor }} />
     </div>
   );
